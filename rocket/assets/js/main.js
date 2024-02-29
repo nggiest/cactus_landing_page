2a3fa37385ef0b3ea96b9f9c92c01768883b4b37
Giovanni <gioanggiesta@gmail.com>
2024-03-01 03:21:03 +0700

    cactus and rocket landing page

---
+++ rocket/assets/js/main.js
@@ -0,0 +1,35 @@
+/*=============== SHOW MENU ===============*/
+var navMenu = document.getElementById("nav-menu");
+var navToggle = document.getElementById("nav-toggle");
+var navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+/*=============== GSAP ANIMATION ===============*/
+gsap.from(".home__points", 1.5, { opacity: 0, y: -300, delay: 0.2 });
+gsap.from(".home__rocket", 1.5, { opacity: 0, y: 300, delay: 0.3 });
+gsap.from(".home__planet-1", 1.5, { opacity: 0, x: 200, delay: 0.8 });
+gsap.from(".home__planet-2", 1.5, { opacity: 0, x: 200, delay: 1 });
+gsap.from(".home__cloud-1", 1.5, { opacity: 0, y: 200, delay: 1.2 });
+gsap.from(".home__cloud-2", 1.5, { opacity: 0, x: 200, delay: 1.3 });
+gsap.from(".home__content", 1.5, { opacity: 0, y: -100, delay: 1.4 });
+gsap.from(".home__title img", 1.5, { opacity: 0, x: -100, delay: 1.4 });
